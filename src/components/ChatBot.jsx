77f5d40d095c9ae9acf2ef597a01f00bb06caeff
Hirushan <kavinduhirushan78@gmail.com>
2026-02-16 21:22:09 +0530

    Update  Chatbot Port

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -21,7 +21,7 @@ const ChatBot = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/chat", {
+      const response = await fetch("http://127.0.0.1:8000/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ elder_id: elderId, message }), // send elder_id
